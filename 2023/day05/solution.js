973e43ce5f911d5050cbe5af6822e7dceafae1bd
Bence Szegvari <bence.szegvari@gmail.com>
2023-12-05 21:26:42 +0100

    feature: 2023 day 05 solution 1

--- 2023/day05/solution.js
+++ 2023/day05/solution.js
@@ -5,6 +5,24 @@ window.onload = () => {
     new InputRegister(DAY_INDEX, YEAR_INDEX).init()
 };
 
+const mapAlmanac = (input) => {
+    const almanac = []
+    const seeds = []
+    let mapIndex = -1
+    input.forEach((line, index) => {
+        if (index === 0) {
+            seeds.push(...line.split(': ')[1].split(' ').map(Number))
+        } else if (line.includes('map')) {
+            almanac.push([])
+            mapIndex++
+        } else if (line.length) {
+            almanac[mapIndex].push(line.split(' ').map(Number))
+        }
+    })
+
+    return [seeds, almanac]
+}
+
 class Solution1 extends OutputCalculator {
     constructor() { super(DAY_INDEX, YEAR_INDEX) }
 
@@ -13,10 +31,26 @@ class Solution1 extends OutputCalculator {
         const parsedInput = this.parseInput({ integerOnly: false, splitBy: '\r\n' })
 
         // Solution
-        const lines = [...parsedInput]
+        const [seeds, almanac]= mapAlmanac(parsedInput)
+
+        const locations = []
+        for (let seed of seeds) {
+            let location = seed
+            for (let i = 0; i < almanac.length; i++) {
+                for (let assignment of almanac[i]) {
+                    const [to, from, range] = assignment
+
+                    if (location >= from && location <= from + range - 1) {
+                        location = to + (location - from)
+                        break;
+                    }
+                }
+            }
+            locations.push(location)
+        }
 
         // Accumulate values
-        const output = lines.reduce((acc,val) => acc + val)
+        const output = Math.min(...locations)
 
         // Fill output field
         this.fillOutput(output)
@@ -31,10 +65,33 @@ class Solution2 extends OutputCalculator {
         const parsedInput = this.parseInput({ integerOnly: false, splitBy: '\r\n' })
 
         // Solution
-        const lines = [...parsedInput]
+        const [seedRange, almanac]= mapAlmanac(parsedInput)
+        const seeds = []
+        // @TODO invalid array length error. Need a different approach
+        for (let i = 0; i < seedRange.length; i++) {
+            if (i === 0 && i % 2 === 0) {
+                for (let j = 0; j < seedRange[i + 1]; j++)
+                    seeds.push(seedRange[i] + j)
+            }
+        }
+        const locations = []
+        for (let seed of seeds) {
+            let location = seed
+            for (let i = 0; i < almanac.length; i++) {
+                for (let assignment of almanac[i]) {
+                    const [to, from, range] = assignment
+
+                    if (location >= from && location <= from + range - 1) {
+                        location = to + (location - from)
+                        break;
+                    }
+                }
+            }
+            locations.push(location)
+        }
 
         // Accumulate values
-        const output = lines.reduce((acc,val) => acc + val)
+        const output = Math.min(...locations)
 
         // Fill output field
         this.fillOutput(output)
---
+++ Templates/day01/solution.js
@@ -0,0 +1,42 @@
+const DAY_INDEX = 1
+const YEAR_INDEX = 2023
+
+window.onload = () => {
+    new InputRegister(DAY_INDEX, YEAR_INDEX).init()
+};
+
+class Solution1 extends OutputCalculator {
+    constructor() { super(DAY_INDEX, YEAR_INDEX) }
+
+    calculate() {
+        // Create array from lines
+        const parsedInput = this.parseInput({ integerOnly: false, splitBy: '\r\n' })
+
+        // Solution
+        const lines = [...parsedInput]
+
+        // Accumulate values
+        const output = lines.reduce((acc,val) => acc + val)
+
+        // Fill output field
+        this.fillOutput(output)
+    }
+}
+
+class Solution2 extends OutputCalculator {
+    constructor() { super(DAY_INDEX, YEAR_INDEX); }
+
+    calculate() {
+        // Create array from lines
+        const parsedInput = this.parseInput({ integerOnly: false, splitBy: '\r\n' })
+
+        // Solution
+        const lines = [...parsedInput]
+
+        // Accumulate values
+        const output = lines.reduce((acc,val) => acc + val)
+
+        // Fill output field
+        this.fillOutput(output)
+    }
+}
